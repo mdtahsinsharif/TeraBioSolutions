b68b773e68b8b73f1c67d3f3c62a579f52915d49
Md Tahsin Sharif <mt2shari@uwaterloo.ca>
2016-02-21 19:26:13 -0500

    V1
    2-21-2016 (version 1)

---
+++ app/src/main/java/com/example/mdtahsinsharif/terabiosolutions/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mdtahsinsharif.terabiosolutions;
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView)findViewById(R.id.logo);
+        image.setImageResource(R.drawable.dices);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IndexPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
